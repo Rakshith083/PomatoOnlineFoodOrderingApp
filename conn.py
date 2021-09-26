7ec3b094b7a504f76c5b7dff95b2c1c813020bbf
Rakshith N <89148712+Rakshith083@users.noreply.github.com>
2021-09-26 17:53:05 +0530

    Update conn.py

--- conn.py
+++ conn.py
@@ -1,8 +1,7 @@
 import pymongo
 
 def mongoConnect():
-    # conString="mongodb://localhost:27017/"
-    conString="mongodb+srv://rakshith:rakshi12@cluster0.grxrq.mongodb.net/food"
+    conString="mongodb://localhost:27017/"
     myclient = pymongo.MongoClient(conString,tlsAllowInvalidCertificates=True)
     if myclient.server_info():
         print("Connected")
